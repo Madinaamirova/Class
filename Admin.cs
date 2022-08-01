0c626f7728889b18f9c62e37e11279b6fa310df2
Madina <madiinaamiirova@gmail.com>
2022-08-02 02:29:47 +0400

    commit

---
+++ Admin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Admin : User
+    {
+        private bool _isSuperAdmin;
+        private string _section;
+        public bool IsSuperAdmin
+        {
+            get { return _isSuperAdmin; }
+            set { _isSuperAdmin = value; }
+        }
+        public string Section
+        {
+            get { return _section; }
+            set { _section = value; }
+        }
+
+        public Admin(string userName, string password, bool isSuperAdmin, string section) : base(userName, password)
+        {
+            IsSuperAdmin = isSuperAdmin;
+            Section = section;
+
+        }
+
+        public override void Info()
+        {
+            base.Info();
+            Console.WriteLine($"IsSuperAdmin:{IsSuperAdmin} Section:{Section} ");
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+            string UserName;
+            string Password;
+
+            do
+            {
+                Console.WriteLine("UserName daxil edin");
+                UserName = Console.ReadLine();
+                if (string.IsNullOrEmpty(UserName) || UserName.Length < 6)
+                {
+                    Console.WriteLine("Sehv daxil edilib");
+                }
+
+            } while (string.IsNullOrEmpty(UserName) || UserName.Length < 6);
+
+            do
+            {
+                Console.WriteLine("Passord daxil edin");
+                Password = Console.ReadLine();
+                if (!User.PasswordChek(Password))
+                {
+                    Console.WriteLine("Sehv daxil edilib");
+                }
+
+            } while (!User.PasswordChek(Password));
+
+            Admin admin = new Admin(UserName, Password, true, "sistem");
+            admin.Info();
+            #endregion
+        }
+    }
+}
